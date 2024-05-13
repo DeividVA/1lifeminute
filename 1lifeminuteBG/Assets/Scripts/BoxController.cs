df166b478095599d447035b72f0920c9d14034cf
DeividVA <david.verdugoabad@gmail.com>
2024-05-13 19:20:47 +0200

    Animations

---
+++ 1lifeminuteBG/Assets/Scripts/BoxController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Disapear());
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //RevealTrueSelf();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        //RevealTrueSelf();
+    }
+    IEnumerator Disapear()
+    {
+        yield return new WaitForSeconds(5);
+        DestroyThis();
+    }
+
+    void DestroyThis()
+    {
+        Destroy(gameObject);
+    }
+}
